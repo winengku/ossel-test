ffd8dafbde7400112e44b024f01212f71bb1a9ee
Witantra <winengkuu@gmail.com>
2020-02-28 04:10:00 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Content></Content>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Content,
+    Footer
+  }
+}
+</script>
