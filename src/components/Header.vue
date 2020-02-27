4946e9d870a640d3979d7e35077387b9229018ad
Witantra <winengkuu@gmail.com>
2020-02-28 06:33:07 +0700

    create header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        Header
+    <div class="header-container">
+        <div class="row mx-0 px-0">
+            <div class="col-5 px-0 logo-container">
+                <img src="../assets/images/logo.jpg">
+            </div>
+            <div class="row col-7 justify-content-end align-items-center mx-0 search-bar-container">
+                search
+            </div>
+        </div>
     </div>
 </template>
