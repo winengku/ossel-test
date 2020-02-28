5a34ec6be91363c20e649d7ecc12e2cf2c50d3f9
Witantra <winengkuu@gmail.com>
2020-02-28 16:15:52 +0700

    checkpoint 2

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="row mx-0 navbar-container align-items-center">
+        <div class="col-5 mx-0">
+            <a href="#"> PRODUCT CATEGORIES <i class="ml-5 fa fa-caret-down"></i> </a>
+        </div>
+        <div class="col-7 mx-0">
+            <nav class="navbar navbar-expand-lg">
+                <ul class="navbar-nav">
+                    <li v-for="routes in links" :key="routes.id" class="nav-item mr-80"><a href=#> {{routes.text}} </a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          text: 'BEST SELLER',
+          page:'#'
+        },
+        {
+          id: 1,
+          text: 'ABOUT US',
+          page:'#'
+        },
+        {
+          id: 2,
+          text: 'CONTACT',
+          page:'#'
+        }
+      ]
+    }
+  }
+}
+</script>
