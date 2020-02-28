fa47185e3734f2e427607629a64b8401a391d17d
Witantra <winengkuu@gmail.com>
2020-02-28 19:41:09 +0700

    create content > our product

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -10,7 +10,7 @@
                 </div>
             </div>
             <div class="row col-7 mx-0 justify-content-center">
-                <div class="col-12 mx-0 mt-3 mb-0 promo-container">
+                <div class="col-12 mx-0 my-3 promo-container">
                     <div class="col-12 mx-0 px-0 py-2 promo-title">
                         Our Promo
                     </div>
@@ -20,6 +20,44 @@
                         <img class="slide" src="../assets/images/promo.jpeg"/>
                         <img class="slide" src="../assets/images/promo.jpeg"/>
                     </agile>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mx-0 justify-content-center pb-3 pt-5 title-content">
+            Our Products
+        </div>
+        <div class="row col-12 justify-content-center mx-0" >
+            <div class="row col-11 mx-0 justify-content-center px-0" style="border:1px solid black;border-radius:20px;">
+                <div class="col-12 mx-0 mt-4 py-2 pl-4 header-article">
+                    Featured Product
+                </div>
+                <div class="col-4 mx- 0">
+                    <img class="object-fit_contain" src="../assets/images/product1.png">
+                    <p class="py-2 mx-5 title-article">GENSET OSSEL RX7500</p>
+                </div>
+                <div class="col-4 mx-0 object-fit_contain">
+                    <img class="object-fit_contain" src="../assets/images/product1.png">
+                    <p class="py-2 mx-5 title-article">GENSET OSSEL RX7500</p>
+                </div>
+                <div class="col-4 mx-0 object-fit_contain">
+                    <img class="object-fit_contain" src="../assets/images/product1.png">
+                    <p class="py-2 mx-5 title-article">GENSET OSSEL RX7500</p>
+                </div>
+                <div class="col-12 mx-0 mt-4 py-2 pl-4 header-article">
+                    Latest Product
+                </div>
+                <div class="col-4 mx- 0">
+                    <img class="object-fit_contain" src="../assets/images/product2.jpg">
+                    <p class="py-2 mx-5 title-article">JIGSAW P-431</p>
+                </div>
+                <div class="col-4 mx-0 object-fit_contain">
+                    <img class="object-fit_contain" src="../assets/images/product2.jpg">
+                    <p class="py-2 mx-5 title-article">JIGSAW P-431</p>
+                </div>
+                <div class="col-4 mx-0 object-fit_contain">
+                    <img class="object-fit_contain" src="../assets/images/product2.jpg">
+                    <p class="py-2 mx-5 title-article">JIGSAW P-431</p>
                 </div>
             </div>
         </div>
